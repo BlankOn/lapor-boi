7bf1541dda18abec900326b133509ea66d90932a
Sofyan Sugianto <sofyanartem@gmail.com>
2020-10-31 16:07:47 +0700

    nonaktifkan tipelaporan layanan dan tim di form

--- src/components/form.js
+++ src/components/form.js
@@ -16,6 +16,8 @@ class FormLapor extends Component {
             judul: null,
             deskripsi: null,
             isSubmitting: false,
+            afterSubmit: false,
+            errors: null
         }
         this.onFormSubmit = this.onFormSubmit.bind(this)
         this.onFieldChanged = this.onFieldChanged.bind(this)
@@ -28,7 +30,26 @@ class FormLapor extends Component {
         newState.isSubmitting = true
         this.setState(newState)
         this.doUploadForm().then((response) => {
-            console.log(response)
+            newState = {
+                lampiran: null,
+                nama: null,
+                email: null,
+                github: null,
+                // tipe_laporan: null,
+                // layanan: null,
+                // tim: null,
+                judul: null,
+                deskripsi: null,
+                isSubmitting: false,
+                afterSubmit: true,
+                errors: null
+            }
+            if (response.error){
+                newState.errors = response.pesan
+            }
+            setTimeout(() => {
+                this.setState(newState)
+            }, 2000);
         })
     }
 
@@ -39,53 +60,67 @@ class FormLapor extends Component {
         }else{
             newState[e.target.name] = e.target.value
         }
-        this.setState(newState, () => {
-            console.log(this.state)
-        })
+        this.setState(newState)
     }
 
-    doUploadForm(){
-        setTimeout(() => {
-            let newState = {
-                lampiran: null,
-                nama: null,
-                email: null,
-                github: null,
-                tipe_laporan: null,
-                layanan: null,
-                tim: null,
-                judul: null,
-                deskripsi: null,
-                isSubmitting: false,
-            }
-            this.setState(newState)
-        }, 1000);
-        return new Promise({error: false, message: "hello world"})
+    async doUploadForm(){
+      const url = process.env.SITE_URL + '/api/submit-ticket'
+      const data = new FormData()
+      if (this.state.lampiran){
+        data.append('lampiran',this.state.lampiran)
+      }
+      data.append('nama',this.state.nama)
+      data.append('email',this.state.email)
+      data.append('github',this.state.github)
+      // data.append('tipe_laporan',this.state.tipe_laporan)
+      // data.append('layanan',this.state.layanan)
+      // data.append('tim',this.state.tim)
+      data.append('judul',this.state.judul)
+      data.append('deskripsi',this.state.deskripsi)
+      return await fetch(url, {
+        method: 'POST',
+        body: data
+      })
+    }
+
+    showLogStatus(){
+        if (!this.state.afterSubmit) return
+        
+        if (this.state.errors === null || this.state.errors === undefined){
+            return (
+                <div className="alert alert-success">Sukses!</div>
+            )
+        }
+        return (
+            <div className="alert alert-danger">{this.state.errors}</div>
+        )
     }
 
     render() {
         if (this.state.isSubmitting) {
             return (<div>Mengirimkan laporan tiket...</div>)
-        }else{
+        }
+
         return (
             <Form onSubmit={this.onFormSubmit}>
+                {this.showLogStatus()}
                 <h3>Informasi Laporan</h3>
                 <Card className="FormLapor">
                   <Form.Row>
                     <Col lg={4} className="mb-2 mb-lg-0">
                       <Form.Label htmlFor="nama">Nama Anda</Form.Label>
-                      <Form.Control onChange={this.onFieldChanged} type="text" name="nama" />
+                      <Form.Control required onChange={this.onFieldChanged} type="text" name="nama" />
                     </Col>
                     <Col lg={4} className="mb-2 mb-lg-0">
                       <Form.Label htmlFor="email">Alamat Email</Form.Label>
-                      <Form.Control onChange={this.onFieldChanged} type="email" name="email" />
+                      <Form.Control required onChange={this.onFieldChanged} type="email" name="email" />
                     </Col>
                     <Col lg={4} className="mb-2 mb-lg-0">
                       <Form.Label htmlFor="github">Username Github</Form.Label>
-                      <Form.Control onChange={this.onFieldChanged} type="text" name="github" />
+                      <Form.Control required onChange={this.onFieldChanged} type="text" name="github" />
                     </Col>
                   </Form.Row>
-                  <Form.Row className="mt-3">
+                  {/* <Form.Row className="mt-3">
                     <Col lg={4} className="mb-2 mb-lg-0">
                       <Form.Label htmlFor="tipe_laporan">Tipe Laporan</Form.Label>
                       <Form.Control onChange={this.onFieldChanged} name="tipe_laporan" as="select">
@@ -102,20 +137,20 @@ class FormLapor extends Component {
                       <Form.Label htmlFor="tim">Tim BlankOn</Form.Label>
                       <Form.Control onChange={this.onFieldChanged} type="text" name="tim" />
                     </Col>
-                  </Form.Row>
+                  </Form.Row> */}
                 </Card>
                 <h3>Rincian Laporan</h3>
                 <Card className="FormLapor">
                   <Form.Row>
                     <Col>
                       <Form.Label htmlFor="judul">Judul Laporan</Form.Label>
-                      <Form.Control onChange={this.onFieldChanged} type="text" name="judul" />
+                      <Form.Control required onChange={this.onFieldChanged} type="text" name="judul" />
                     </Col>
                   </Form.Row>
                   <Form.Row className="mt-3">
                     <Col>
                       <Form.Label htmlFor="deskripsi">Deskripsi Laporan</Form.Label>
-                      <Form.Control onChange={this.onFieldChanged} name="deskripsi" rows={10} as="textarea" />
+                      <Form.Control required onChange={this.onFieldChanged} name="deskripsi" rows={10} as="textarea" />
                     </Col>
                   </Form.Row>
                   <Form.Row className="mt-3">
@@ -132,7 +167,7 @@ class FormLapor extends Component {
                 </Form.Row>
             </Form>
         )
-    }}
+    }
 }
 
 export default FormLapor
